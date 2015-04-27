1691094d0e44059ea011e946a86bb9708ba79dd9
Victor Hugo <victor.h.alencar@gmail.com>
2015-04-26 23:33:25 -0300

    implementação do objeto OperacoesBancarias

--- spec/operacoes_bancarias.js
+++ spec/operacoes_bancarias.js
@@ -3,25 +3,45 @@ describe("Sacar dinheiro", function() {
 
   beforeEach(function() {
     operacao = new OperacoesBancarias();
+
+    /*
+    // Mock
+    operacao = jasmine.createSpyObj('OperacoesBancarias',
+      [
+        'set_valor_a_ser_sacado', 'get_valor_a_ser_sacado',
+        'set_primeira_letra', 'set_segunda_letra', 'set_terceira_letra',
+        'get_padrao_letra', 'get_verificar_letras',
+        'sacar'
+      ]);
+    */
+
+    spyOn(operacao, "get_saldo").and.callFake(function(){
+      return 800;
+    });
+
+    spyOn(operacao, "get_padrao_letras_servidor").and.callFake(function(){
+      return ["Jas","Mi","Ne"];
+    });
   });
 
   it("Deve inserir o valor a ser sacado", function() {
     operacao.set_valor_a_ser_sacado(10);
+    expect(operacao.get_valor_a_ser_sacado()).toBe(10);
     expect(operacao.get_valor_a_ser_sacado()).toEqual(jasmine.any(Number));
   });
 
   it("Deve inserir as letras de segurança", function() {
-    operacao.set_primeiro_padrao_letra("Jas");
-    operacao.set_segunda_padrao_letra("Mi");
-    operacao.set_terceira_padrao_letra("Ne");
+    operacao.set_primeira_letra("Jas");
+    operacao.set_segunda_letra("Mi");
+    operacao.set_terceira_letra("Ne");
 
-    expect(operacao.get_padrao_letra()).toBe(["Jas","Mi","Ne"]);
+    expect(operacao.get_padrao_letra()).toEqual(["Jas","Mi","Ne"]);
   });
 
   it("Deve verificar as letras de segurança", function() {
-    operacao.set_primeiro_padrao_letra("Jas");
-    operacao.set_segunda_padrao_letra("Mi");
-    operacao.set_terceira_padrao_letra("Ne");
+    operacao.set_primeira_letra("Jas");
+    operacao.set_segunda_letra("Mi");
+    operacao.set_terceira_letra("Ne");
 
     expect(operacao.get_verificar_letras()).toBe(true);
   });
@@ -29,9 +49,9 @@ describe("Sacar dinheiro", function() {
   it("Deve sacar o dinheiro", function() {
     operacao.set_valor_a_ser_sacado(200);
 
-    operacao.set_primeiro_padrao_letra("Jas");
-    operacao.set_segunda_padrao_letra("Mi");
-    operacao.set_terceira_padrao_letra("Ne");
+    operacao.set_primeira_letra("Jas");
+    operacao.set_segunda_letra("Mi");
+    operacao.set_terceira_letra("Ne");
 
     expect(operacao.get_valor_a_ser_sacado()).toEqual(jasmine.any(Number));
     expect(operacao.get_verificar_letras()).toBe(true);
@@ -46,9 +66,9 @@ describe("Sacar dinheiro", function() {
     });
 
     it("Não deve sacar o dinheiro se as letras de segurança não forem válidas", function() {
-      operacao.set_primeiro_padrao_letra("Jas");
-      operacao.set_segunda_padrao_letra("Mu");
-      operacao.set_terceira_padrao_letra("Ni");
+      operacao.set_primeira_letra("Jas");
+      operacao.set_segunda_letra("Mu");
+      operacao.set_terceira_letra("Ni");
 
       expect(operacao.get_verificar_letras()).not.toBe(true);
       expect(operacao.sacar()).toBe(false);
@@ -57,9 +77,9 @@ describe("Sacar dinheiro", function() {
     it("Não deve sacar o dinheiro se o saldo for insuficiente", function() {
       operacao.set_valor_a_ser_sacado(10000);
 
-      operacao.set_primeiro_padrao_letra("Jas");
-      operacao.set_segunda_padrao_letra("Mi");
-      operacao.set_terceira_padrao_letra("Ne");
+      operacao.set_primeira_letra("Jas");
+      operacao.set_segunda_letra("Mi");
+      operacao.set_terceira_letra("Ne");
 
       expect(operacao.get_valor_a_ser_sacado()).toBe(10000);
       expect(operacao.get_verificar_letras()).toBe(true);
@@ -70,10 +90,10 @@ describe("Sacar dinheiro", function() {
 
 describe("Consultar saldo", function() {
   it("Deve verificar as letras de segurança", function() {
-    
+
   });
-  
-  it("Deve exibir o saldo atual para o usuário", function() {
-    
+
+  xit("Deve exibir o saldo atual para o usuário", function() {
+
   });
 });
--- src/OperacoesBancarias.js
+++ src/OperacoesBancarias.js
@@ -1,2 +1,46 @@
 function OperacoesBancarias() {
+  this.valor_a_ser_sacado = null;
+  this.primeira_letra = "";
+  this.segunda_letra = "";
+  this.terceira_letra = "";
+
+  this.set_valor_a_ser_sacado = function(valor) {
+    this.valor_a_ser_sacado = valor;
+  };
+
+  this.get_valor_a_ser_sacado = function() {
+    return this.valor_a_ser_sacado;
+  };
+
+  this.set_primeira_letra = function(primeira_letra) {
+    this.primeira_letra = primeira_letra;
+  };
+
+  this.set_segunda_letra = function(segunda_letra) {
+    this.segunda_letra = segunda_letra;
+  };
+
+  this.set_terceira_letra = function(terceira_letra) {
+    this.terceira_letra = terceira_letra;
+  };
+
+  this.get_padrao_letra = function() {
+    return [this.primeira_letra, this.segunda_letra, this.terceira_letra];
+  }
+
+  this.get_padrao_letras_servidor = function() {
+    // deve fazer uma requisição ao servidor para saber quais são as letras padrões
+  };
+
+  this.get_verificar_letras = function() {
+    return this.get_padrao_letras_servidor().equals(this.get_padrao_letra());
+  };
+
+  this.get_saldo = function() {
+    // deve fazer uma requisição ao servidor para saber qual o saldo atual
+  }
+
+  this.sacar = function() {
+    return (this.valor_a_ser_sacado > 0 && this.valor_a_ser_sacado <= this.get_saldo() && this.get_verificar_letras());
+  };
 }
